59d28efa395477af05148e4d3f9dd7e651e34e53
SMDindl <SDINDL@email.sc.edu>
2024-10-05 16:52:09 -0400

    began on data management elements

---
+++ LearningApplication/DataConstants.java
@@ -0,0 +1,37 @@
+
+/**
+ * 
+ */
+public class DataConstants {
+
+    // User class information (list contained in UserList)
+    protected static final String USER_FILE_NAME = "json\\Users.json";
+	protected static final String USER_ID = "id";
+	protected static final String USER_USER_NAME = "userName";
+	protected static final String DISPLAY_NAME = "displayName";
+	protected static final String EMAIL = "email";
+	protected static final String PASSWORD = "password";
+    protected static final String PROGRESS_TRACKERS = "progressTrackers"; // May need to be reworked
+
+    // Progress Trackers class information (list contained in User)
+
+    // GameData class information (lists contained in GameData)
+    // GameKey info
+    // Language(s)
+    protected static final String FILIPINO = "filipino";
+
+    // Difficulties
+    protected static final String EASY = "easy";
+    protected static final String MEDIUM = "medium";
+    protected static final String HARD = "hard";
+
+    // Games
+    protected static final String COLORS_GAME = "colorsGame";
+    protected static final String STORIES_GAME = "storiesGame";
+    protected static final String ALPHABET_GAME = "alphabetGame";
+    protected static final String NUMBERS_GAME = "numbersGame";
+
+    // Data Types
+    // Questions class information (list contained in GameData)
+
+}
