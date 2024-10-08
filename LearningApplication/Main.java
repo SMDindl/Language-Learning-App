8278365c14a6d269037e4983f361fb5cb836a5ec
SMDindl <SDINDL@email.sc.edu>
2024-10-08 12:42:16 -0400

    Working DataLoader and Main (Tester class); dataLoader shows json simple not found but code is still runnning

--- LearningApplication/Main.java
+++ LearningApplication/Main.java
@@ -6,16 +6,19 @@ public class Main extends DataConstants {
 
     public static void main(String args[]) {
 
+
+        // load data
+        DataLoader dataLoader = new DataLoader();
+        GameData gameData = GameData.getInstance();
+        dataLoader.loadGameData();
+
+        // Set GameKey
         String language = DataConstants.FILIPINO;
         String difficulty = DataConstants.EASY;
         String gameType = DataConstants.COLORS_GAME;
-
-        DataLoader dataLoader = new DataLoader();
-        GameData gameData = GameData.getInstance();
         DataKey dataKey = DataKey.getInstance(language, gameType, difficulty);
-        dataLoader.loadGameData();
-
-        // Test dataKey
+        
+        // Test dataKey with loaded data
         gameType = DataConstants.ALPHABET_GAME;
         dataKey.setGameType(gameType);
         System.out.println("KEY: " + dataKey.toString());
