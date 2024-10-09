498b0fcf1847d18d152ff99b51b0ae45291930e5
Duncan Szabaga <dszabaga@email.sc.edu>
2024-10-09 18:00:35 -0400

    Most updated version of StoriesGame class, with method outlined but not fully implemented

--- LearningApplication/StoriesGame.java
+++ LearningApplication/StoriesGame.java
@@ -7,6 +7,32 @@ public class StoriesGame {
 
     public void startGame() {
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("What would you like to do?: \n1. Take a story quiz \n2. Return to game selection");
+        int prompt = keyboard.nextInt();
+        if (prompt == 2) {
+            //Implement this to go back to the game selection
+        }
+
+        //User picks the story to work on
+        Story selection = pickStory();
+        teachWords(selection);
+        List<Page> storyPages = selection.getPages();
+        for(int i = 0; i < storyPages.size(); i++) {
+            storyPages.get(i).getContent();
+        }
+        System.out.println("When you're ready for the quiz, hit the ENTER key.");
+        keyboard.next();
+
+        //Questions for after the user is done with the story
+        Question newQuestion = askQuestion(selection);
+        if(validateAnswer(keyboard.nextLine(), newQuestion) == true)
+            System.out.println("well done!");
+        else
+            System.out.println("Better luck next time!");
+    }
+
+    public Story pickStory() {
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("\nWhich story would you like to use?: \n");
         DataKey dataKey = null; //Temporary variable just to remove error in following line
         List<Story> storyList = gameData.getStories(dataKey);
@@ -15,12 +41,7 @@ public class StoriesGame {
             System.out.println((i+1) + ". " + storyList.get(i).getTitle());
         }
         Story selection = storyList.get(keyboard.nextInt() - 1);
-        teachWords(selection);
-        List<Page> storyPages = selection.getPages();
-        for(int i = 0; i < storyPages.size(); i++) {
-            storyPages.get(i).getContent();
-        }
-        askQuestion(selection);
+        return selection;
     }
 
     public void teachWords(Story story) {
