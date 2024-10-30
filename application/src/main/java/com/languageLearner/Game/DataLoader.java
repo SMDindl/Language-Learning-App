0aa2a04eacc724a936f70aed78e326a476b895ef
SMDindl <SDINDL@email.sc.edu>
2024-10-30 01:30:44 -0400

    Testing / not loading

--- application/src/main/java/com/languageLearner/Game/DataLoader.java
+++ application/src/main/java/com/languageLearner/Game/DataLoader.java
@@ -1,76 +1,136 @@
 package com.languageLearner.game;
 
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.UUID;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class DataLoader {
 
-    private static DataLoader instance;
+    private final GameData gameData = GameData.getInstance();
 
-    // Singleton
-    // Clear all ArrayLists/HashMaps before loading
-    private DataLoader() {
-
-    }
-
-    public DataLoader getInstance() {
-        if (instance == null) {
-            instance = new DataLoader();
-        }
-        
-        return instance;
-    }
-
-    // Method to load all games from a JSON file
-    public static ArrayList<Game> loadGames(String filePath) {
-        ArrayList<Game> games = new ArrayList<>();
+    public void loadData(String jsonString) {
         JSONParser parser = new JSONParser();
-        
-        try (FileReader reader = new FileReader(filePath)) {
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
-            String language = (String) jsonObject.get("LANG");  // Retrieves the language as a String
-            UUID languageUUID = UUID.fromString((String) jsonObject.get("UUID"));  // Retrieves UUID as a String and converts to UUID
+        try {
+            // Parse the JSON string into a JSONObject
+            JSONObject jsonData = (JSONObject) parser.parse(jsonString);
 
-            GameData.getInstance().addLanguage(languageUUID, language); // add to GameData language
+            // Get the "LANGUAGES" array
+            JSONArray languagesArray = (JSONArray) jsonData.get("LANGUAGES");
 
-            JSONArray gamesArray = (JSONArray) jsonObject.get("GAMES");
+            for (Object languageObj : languagesArray) {
+                JSONObject language = (JSONObject) languageObj;
 
-            for (Object gameObj : gamesArray) {
-                JSONObject gameJson = (JSONObject) gameObj;
-                Game game = Game.fromJson(gameJson, language, languageUUID);
-                GameData.getInstance().addGame(game); // add to GameData language
+                // Retrieve language and UUID
+                String languageName = (String) language.get("LANG");
+                String languageUUIDString = (String) language.get("UUID");
+
+                if (languageName == null || languageUUIDString == null) continue;
+                UUID languageUUID = UUID.fromString(languageUUIDString);
+
+                // Add language to GameData
+                gameData.addLanguage(languageUUID, languageName);
+
+                // Process GAMES array within each language
+                JSONArray gamesArray = (JSONArray) language.get("GAMES");
+                if (gamesArray != null) {
+                    loadGames(gamesArray, languageUUID, languageName);
+                }
             }
-        } catch (Exception e) {
+
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-        
-        return games;
     }
 
-    // // Method to load all questions from a JSON file
-    // public static List<Question> loadQuestions(String filePath) {
-    //     List<Question> questions = new ArrayList<>();
-    //     JSONParser parser = new JSONParser();
-        
-    //     try (FileReader reader = new FileReader(filePath)) {
-    //         JSONObject jsonObject = (JSONObject) parser.parse(reader);
-    //         JSONArray questionsArray = (JSONArray) jsonObject.get("questions");
+    private void loadGames(JSONArray gamesArray, UUID languageUUID, String languageName) {
+        for (Object gameObj : gamesArray) {
+            JSONObject game = (JSONObject) gameObj;
 
-    //         for (Object questionObj : questionsArray) {
-    //             JSONObject questionJson = (JSONObject) questionObj;
-    //             Question question = Question.fromJson(questionJson);
-    //             questions.add(question);
-    //         }
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-        
-    //     return questions;
-    // }
+            // Retrieve game details
+            String gameName = (String) game.get("GAME");
+            String difficulty = (String) game.get("DIFF");
+            String gameUUIDString = (String) game.get("UUID");
+
+            if (gameName == null || difficulty == null || gameUUIDString == null) continue;
+            UUID gameUUID = UUID.fromString(gameUUIDString);
+
+            // Parse GameInfo if present
+            JSONObject infoObj = (JSONObject) game.get("INFO");
+            GameInfo gameInfo = null;
+            if (infoObj != null) {
+                gameInfo = parseGameInfo(infoObj, gameUUID);
+                if (gameInfo != null) {
+                    gameData.addGameInfo(gameInfo); // Add GameInfo to GameData
+                }
+            }
+
+            // Parse TEXT array and add each TextObject to GameData
+            JSONArray textArray = (JSONArray) game.get("TEXT");
+            ArrayList<TextObject> textObjects = new ArrayList<>();
+            if (textArray != null) {
+                for (Object textObj : textArray) {
+                    JSONObject text = (JSONObject) textObj;
+                    TextObject textObject = parseTextObject(text, gameUUID);
+                    if (textObject != null) {
+                        textObjects.add(textObject);
+                        gameData.addTextObject(textObject); // Add TextObject to GameData
+                    }
+                }
+            }
+
+            // Create Game object
+            Game newGame = new Game(languageName, languageUUID, gameName, difficulty, gameUUID, gameInfo, textObjects);
+            gameData.addGame(newGame); // Add Game to GameData
+        }
+    }
+
+    private GameInfo parseGameInfo(JSONObject infoObj, UUID gameUUID) {
+        // Parse fields from the INFO object
+        String description = (String) infoObj.get("description");
+        String objective = (String) infoObj.get("objective");
+        JSONArray instructionsArray = (JSONArray) infoObj.get("instructions");
+
+        // Convert instructions JSONArray to ArrayList<String>
+        ArrayList<String> instructions = new ArrayList<>();
+        if (instructionsArray != null) {
+            for (Object instruction : instructionsArray) {
+                instructions.add(instruction.toString());
+            }
+        }
+
+        String introConcept = (String) ((JSONObject) infoObj.get("prep")).get("introConcept");
+        String exampleUsage = (String) ((JSONObject) infoObj.get("prep")).get("exampleUsage");
+        String gameTip = (String) ((JSONObject) infoObj.get("prep")).get("gameTip");
+
+        // Create and return GameInfo
+        GameInfo gameInfo = new GameInfo(description, objective, instructions, introConcept, exampleUsage, gameTip, gameUUID);
+        return gameInfo;
+    }
+
+    private TextObject parseTextObject(JSONObject textObj, UUID gameUUID) {
+        // Parse fields for the TextObject
+        String text = (String) textObj.get("text");
+        String englishText = (String) textObj.get("englishText");
+        String linkedText = (String) textObj.get("linkedText");
+        String englishLinkedText = (String) textObj.get("englishLinkedText");
+        String helperText = (String) textObj.get("helperText");
+        String textUUIDString = (String) textObj.get("UUID");
+
+        if (text == null || englishText == null || textUUIDString == null) return null;
+        UUID textUUID = UUID.fromString(textUUIDString);
+
+        // Create and return TextObject
+        TextObject textObject = new TextObject(text, englishText, linkedText, englishLinkedText, helperText, textUUID, gameUUID);
+        return textObject;
+    }
+
+    public static void main(String[] args) {
+        DataLoader loader = new DataLoader();
+        loader.loadDataFromFile("data.json"); 
+    }
 }
--- application/src/test/java/TestDataLoader.java
+++ application/src/test/java/TestDataLoader.java
@@ -1,12 +1,19 @@
 
+import java.util.ArrayList;
+
 import com.languageLearner.game.DataLoader;
+import com.languageLearner.game.Game;
 import com.languageLearner.game.GameData;
 
 public class TestDataLoader {
 
     public static void main(String[] args) {
         DataLoader.loadGames("json\\gamesData.json");
-        System.out.println(GameData.getInstance().getGamesWithGameIDs());
+        GameData gameData = GameData.getInstance();
+        ArrayList<Game> games = gameData.getGames();
+        for(int i = 0; i < games.size(); i++) {
+            System.out.println(games.get(i));
+        }
     }
     
 }
