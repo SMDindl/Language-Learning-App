c8ba0f73aa71ab19f23b07d089f4408bb69b128b
SMDindl <SDINDL@email.sc.edu>
2024-10-30 10:29:39 -0400

    Began on a Language class, needs implemented, started layout of gamesQuestions json

---
+++ application/src/main/java/com/languageLearner/Game/Language.java
@@ -0,0 +1,15 @@
+package com.languageLearner.game;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.languageLearner.app.Question;
+
+public class Language {
+
+    private String languageName;
+    private UUID uuid;
+    private ArrayList<Game> games;
+    private ArrayList<Question> questions;
+
+}
