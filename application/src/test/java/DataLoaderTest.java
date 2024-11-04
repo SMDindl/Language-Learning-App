fbadc349d4b612d1e2b3d0ad3b7f57f294f1cc52
SMDindl <SDINDL@email.sc.edu>
2024-11-03 20:36:59 -0500

    Add test classes, updated user.json

--- application/src/test/java/DataLoaderTest.java
+++ application/src/test/java/DataLoaderTest.java
@@ -1,3 +1,20 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class DataLoaderTest {
-    
+
+    @Test
+    public void testLoadingGameData() {
+        
+    }
+
+    @Test
+    public void testLoadingUserData() {
+        
+    }
+
 }
